0871bca6c208812301c7656f938e5dcf37cb1f82
root <root@ip-172-31-17-217.ec2.internal>
2023-03-07 09:49:17 +0000

    Add project skeleton

--- src/main/java/com/Bookzy/webapp/WebappApplication.java
+++ src/main/java/com/Bookzy/webapp/WebappApplication.java
@@ -1,4 +1,4 @@
-package com.bookzy.webapp;
+package com.Bookzy.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/Bookzy/webapp/WebappApplicationTests.java
+++ src/test/java/com/Bookzy/webapp/WebappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookzy.webapp;
+package com.Bookzy.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
